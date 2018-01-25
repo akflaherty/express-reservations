58c789784c3e251a64a6238aaa3c693b9fb95132
Annie Flaherty <anne.flaherty@case.edu>
2018-01-24 21:18:14 -0500

    Fixed home.html path

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var waitlist = [{
 }];
 
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get("/tables", function(req, res) {
